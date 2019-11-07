1bf6a987585faac5f4f4d80e705ab2a9a13d9e7b
Toby <z5141448@ad.unsw.edu.au>
2019-11-07 21:32:22 +1100

    support the autojump and the fuck

--- install.sh
+++ install.sh
@@ -9,4 +9,13 @@ cat zshrc alias.local >> zshrc.local
 ln zshrc.local ~/.zshrc
 chmod 644 zshrc.local
 
+# install the fuck 
+pip install --user thefuck 
+
+# install autojump 
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
+cd .. && rm autojump
+
 echo "Install zshrc success. "
